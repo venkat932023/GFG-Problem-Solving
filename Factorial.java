611a410e0fc2b6155612ba2ec89a68d1b7c84162
venkat932023 <venkateswarareddyjavvaji16@gmail.com>
2026-08-06 15:39:41 +0530

    Factorial code added

---
+++ Factorial.java
@@ -0,0 +1,24 @@
+// Given a positive integer, n. Find the factorial of n.
+
+// Examples :
+
+// Input: n = 5
+// Output: 120
+// Explanation: 1 x 2 x 3 x 4 x 5 = 120
+// Input: n = 4
+// Output: 24
+// Explanation: 1 x 2 x 3 x 4 = 24
+// Constraints:
+// 0 ≤ n ≤ 12
+
+
+class Solution {
+	int factorial(int n) {
+		int fact = 1;
+		for (int i = 2; i <= n; i++) {
+			fact *= i;
+		}
+		return fact;
+		
+	}
+}
