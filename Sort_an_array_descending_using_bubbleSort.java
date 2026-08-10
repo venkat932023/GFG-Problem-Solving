542881cf7794db6321a5935f5929e951dbe007c6
venkat932023 <venkateswarareddyjavvaji16@gmail.com>
2026-08-10 19:31:15 +0530

    Code added

--- Sort_an_array_descending_using_bubbleSort.java
+++ Sort_an_array_descending_using_bubbleSort.java
@@ -1,11 +1,10 @@
-import java.util.*;
-class Bubble_sort{
+public class Sort_an_array_descending_using_bubbleSort {
     public static void main(String[] args) {
         int[] arr = {2,4,3,65,7,8,3,2,6};
-        for(int i=0; i<arr.length-2; i++){
+        for(int i=0; i<arr.length-1; i++){
             boolean isSorted = true;
             for (int j=0; j<arr.length-i-1; j++){
-                if (arr[j] > arr[j+1]){
+                if (arr[j] < arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
@@ -17,7 +16,5 @@ class Bubble_sort{
             
         }
         System.out.print(Arrays.toString(arr));
-        }
+    }
 }
-
-
