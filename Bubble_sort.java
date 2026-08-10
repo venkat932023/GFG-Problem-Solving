0b3cf950c68509e9495a158c08c5e21fed672c10
venkat932023 <venkateswarareddyjavvaji16@gmail.com>
2026-08-10 17:30:19 +0530

    code added

---
+++ Bubble_sort.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Bubble_sort{
+    public static void main(String[] args) {
+        int[] arr = {2,4,3,65,7,8,3,2,6};
+        for(int i=0; i<arr.length-2; i++){
+            boolean isSorted = true;
+            for (int j=0; j<arr.length-i-1; j++){
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted == true)
+                break;
+            
+        }
+        System.out.print(Arrays.toString(arr));
+        }
+}
