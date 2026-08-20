83359ef8e85395855e46e2ab23997ac62d32ba53
venkat932023 <venkateswarareddyjavvaji16@gmail.com>
2026-08-20 18:34:35 +0530

    code add

---
+++ Sum_of_Substring_of_Number.java
@@ -0,0 +1,35 @@
+// Sum of all substrings of a number
+// Solved
+// Difficulty: MediumAccuracy: 38.11%Submissions: 76K+Points: 4
+// Given an integer s represented as a string, the task is to get the sum of all possible sub-strings of this string.
+
+// Note: The number may have leading zeros.
+// It is guaranteed that the total sum will fit within a 32-bit signed integer.
+
+// Examples:
+
+// Input: s = "6759"
+// Output: 8421
+// Explanation:
+// Sum = 6 + 7 + 5 + 9 + 67 + 75 + 59 + 675 + 759 + 6759 = 8421
+// Input: s = "421"
+// Output: 491
+// Explanation: 
+// Sum = 4 + 2 + 1 + 42 + 21 + 421 = 491
+// Constraints:
+// 1 <= |s| <= 9
+
+
+class Solution {
+    public static int sumSubstrings(String s) {
+        int n = 0;
+        for (int i=0; i<s.length(); i++){
+            for (int j=i+1; j<=s.length(); j++){
+                String m = s.substring(i,j);
+                n += Integer.parseInt(m);
+            }
+        }
+        return n;
+        
+    }
+}
