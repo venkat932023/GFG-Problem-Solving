967c5c73daf47cf3e8ea16b6e58a96db14c32d35
venkat932023 <venkateswarareddyjavvaji16@gmail.com>
2026-08-21 21:14:34 +0530

    code add

---
+++ Anagram.java
@@ -0,0 +1,35 @@
+// Anagram
+// Solved
+// Difficulty: EasyAccuracy: 44.93%Submissions: 501K+Points: 2Average Time: 20m
+// Given two non-empty strings s1 and s2, consisting only of lowercase English letters, determine whether they are anagrams of each other or not.
+// Two strings are considered anagrams if they contain the same characters with exactly the same frequencies, regardless of their order.
+
+// Examples:
+
+// Input: s1 = "geeks" s2 = "kseeg"
+// Output: true 
+// Explanation: Both the string have same characters with same frequency. So, they are anagrams.
+// Input: s1 = "allergy", s2 = "allergyy" 
+// Output: false 
+// Explanation: Although the characters are mostly the same, s2 contains an extra 'y' character. Since the frequency of characters differs, the strings are not anagrams. 
+// Input: s1 = "listen", s2 = "lists" 
+// Output: false 
+// Explanation: The characters in the two strings are not the same — some are missing or extra. So, they are not anagrams.
+// Constraints:
+// 1 ≤ s1.size(), s2.size() ≤ 105
+// s1, s2 consists of lowercase English letters.
+
+
+class Solution {
+    public static boolean areAnagrams(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        for (int i=0; i<arr1.length; i++){
+            if (arr1[i] != arr2[i]) return false;
+        }
+        return true;
+    }
+}
